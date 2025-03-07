8c8dd3151d4179cd6f96df2ea2ce22f9ffdb4a3a
Alexander Risch <alex.j.risch@gmail.com>
2025-03-07 14:12:50 +0700

    Info as Client Page

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,13 +1,21 @@
-import axios from "axios";
+'use client';
 
-export default async function InfoPage() {
-  const res = await axios.get('/api/info');
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function InfoPage() {
+  const [data, setData] = useState<null | unknown>(null)
+  useEffect(() => {
+    axios.get('/api/info').then(res => {
+      setData(res.data)
+    })
+  }, [])
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <pre>
           <code>
-            {JSON.stringify(res.data, null, 2)}
+            {JSON.stringify(data, null, 2)}
           </code>
         </pre>
       </main>
